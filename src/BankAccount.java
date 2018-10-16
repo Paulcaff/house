28a7fbbb7d7ac5d39d51a1df363d0bc10c89ddc4
t00202160 <paul.cafferkey@ittralee.ie>
2018-10-16 17:49:39 +0100

    ff

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,5 +1,37 @@
-public class BankAccount {
+public abstract class BankAccount implements Taxable,Transactable{
 
     protected String name;
     protected int accnum;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAccnum() {
+        return accnum;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAccnum(int accnum) {
+        this.accnum = accnum;
+    }
+
+    public BankAccount (String name, int accnum){
+        setAccnum(accnum);
+        setName(name);
+    }
+
+    public BankAccount(){
+        this.name = this.name;
+        this.accnum = this.accnum;
+    }
+
+
+
+    public String toString() {
+        return "Name: "+name+ "Account Number: "+accnum;
+    }
 }
---
+++ src/SavingsAccount.java
@@ -0,0 +1,45 @@
+public class SavingsAccount extends BankAccount {
+
+    private double balance;
+
+
+    public SavingsAccount(){
+       this ("No Name",0,0);
+
+    }
+
+    public SavingsAccount(String name, int accnum, double balance){
+        super(name, accnum);
+        this.balance = balance;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+
+    public double calcTax() {
+        double tax = 0;
+        double rate = 0.1;
+
+        tax = this.balance * rate;
+        return tax;
+    }
+
+
+    public void lodge(double amount) {
+
+        this.balance += amount;
+    }
+
+
+    public void withdraw(double amount) {
+
+        this.balance -= amount;
+
+    }
+}
